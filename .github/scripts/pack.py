bdac609f716e2a11c083055f4e0a085d5e23d1ac
MuXiu1997 <muxiu1997@gmail.com>
2023-08-14 16:25:39 +0800

    Update packing and zipping process for release workflow

--- .github/scripts/pack.py
+++ .github/scripts/pack.py
@@ -4,7 +4,6 @@ from os import path
 paths_to_be_packed = [
     'config',
     'resources',
-    'scripts',
     'GregTech.lang',
     'GregTech_US.lang',
     'GTNH介绍.txt',
@@ -16,21 +15,23 @@ to_be_renamed = {
     'resources': 'config/txloader/forceload',
 }
 
-to_be_excluded_in_normal = [
-    'scripts',
-]
+base_repo_path = 'repo'
+base_temp_path = 'temp'
 
 
-def copy_to(_path, pack_name):
-    path_in_repo = path.join('repo', _path)
-    path_in_pack = path.join('temp', pack_name, to_be_renamed.get(_path, _path))
-    if path.isdir(path_in_repo):
-        shutil.copytree(path_in_repo, path_in_pack)
+def copy_item(src_path, dest_path):
+    if path.isdir(src_path):
+        shutil.copytree(src_path, dest_path)
     else:
-        shutil.copy(path_in_repo, path_in_pack)
+        shutil.copy(src_path, dest_path)
 
 
-for p in paths_to_be_packed:
-    copy_to(p, 'complete')
-    if p not in to_be_excluded_in_normal:
-        copy_to(p, 'normal')
+def main():
+    for p in paths_to_be_packed:
+        src_path = path.join(base_repo_path, p)
+        dest_path = path.join(base_temp_path, to_be_renamed.get(p, p))
+        copy_item(src_path, dest_path)
+
+
+if __name__ == '__main__':
+    main()
